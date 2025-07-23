3bdf3aca26241a09c78c8a4e35c2ff1a2cbb52a5
Hossein <hosseinrasipooreha@gmail.com>
2025-07-23 13:09:31 +0330

    feat: add admin controller

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using Golestan.Models;
+using Microsoft.AspNetCore.Mvc;
+using UniversityManager.Data;
+
+namespace Golestan.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly GolestanContext _context;
+
+        public AdminController(GolestanContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
